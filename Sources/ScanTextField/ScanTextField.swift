2c7880c73d3c4641ba87c6ebd5bf86a8f05fb8f6
Florian Schweizer <post.an.flo@gmail.com>
2021-09-25 19:00:55 +0200

    Add ScanTextField

--- Sources/ScanTextField/ScanTextField.swift
+++ Sources/ScanTextField/ScanTextField.swift
@@ -1,6 +1,54 @@
-public struct ScanTextField {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct ScanTextField: UIViewRepresentable {
+    @Binding var text: String
+    
+    public func makeUIView(context: Context) -> some UIView {
+        let textFromCamera = UIAction.captureTextFromCamera(responder: context.coordinator, identifier: nil)
+        let image = UIImage(systemName: "text.viewfinder")
+        let toolbarItem = UIBarButtonItem(title: nil, image: image, primaryAction: textFromCamera, menu: nil)
+        
+        let textfield = UITextField()
+        let bar = UIToolbar()
+        bar.items = [toolbarItem]
+        bar.sizeToFit()
+        textfield.inputAccessoryView = bar
+        textfield.placeholder = "Enter name"
+        textfield.delegate = context.coordinator
+        textfield.text = text
+        
+        return textfield
+    }
+    
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        if uiView is  UITextField {
+            (uiView as! UITextField).text = text
+        }
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    public class Coordinator: UIResponder, UIKeyInput, UITextFieldDelegate {
+        public var hasText: Bool {
+            !parent.text.isEmpty
+        }
+        
+        public let parent: ScanTextField
+        
+        public init(_ parent: ScanTextField) {
+            self.parent = parent
+        }
+        
+        public func insertText(_ text: String) {
+            parent.text = text
+        }
+        
+        public func deleteBackward() { }
+        
+        public func textFieldDidBeginEditing(_ textField: UITextField) {
+            parent.text = textField.text ?? ""
+        }
     }
 }
